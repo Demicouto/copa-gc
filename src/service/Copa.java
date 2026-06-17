ee777693c587045e827d31d671153aad3aab5fb2
MarcioFerreiraL <contactmarcioflima@gmail.com>
2026-06-16 22:50:38 -0300

    feat(copa): adiciona registrarPartida e listarGrupo

--- src/service/Copa.java
+++ src/service/Copa.java
@@ -15,4 +15,32 @@ public class Copa {
         this.partidas = new ArrayList<>();
         this.artilheiros = new ArrayList<>();
     }
+
+    public void adicionarSelecao(Selecao selecao) {
+        this.selecoes.add(selecao);
+    }
+
+    public void registrarPartida(Partida partida) {
+        this.partidas.add(partida);
+    }
+
+    public void listarGrupo(char grupo) {
+        System.out.println("=== Grupo " + grupo + " ===");
+        for (Selecao s : selecoes) {
+            if (s.getGrupo() == grupo) {
+                System.out.println(s.toString());
+            }
+        }
+    }
+
+    public void adicionarArtilheiro(String nome) {
+        this.artilheiros.add(nome);
+    }
+
+    public void topArtilheiros() {
+        System.out.println("=== Top Artilheiros ===");
+        for (String artilheiro : artilheiros) {
+            System.out.println(artilheiro);
+        }
+    }
 }
